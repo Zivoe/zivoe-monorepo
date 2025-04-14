7ee86bc50847325e1a3ac9f3461b3fe66b97dbf9
alexsserban <alex.serban.dev@gmail.com>
2025-04-14 18:39:38 +0300

    feat: add infra section

--- apps/landing/src/app/home/index.tsx
+++ apps/landing/src/app/home/index.tsx
@@ -3,6 +3,7 @@ import { Separator } from '@zivoe/ui/core/separator';
 import Diversify from './_components/diversify';
 import Hero from './_components/hero';
 import HowItWorks from './_components/how-it-works';
+import Infrastructure from './_components/infrastructure';
 import Portfolio from './_components/portfolio';
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Separator />
       <HowItWorks />
       <Portfolio />
+      <Infrastructure />
 
       <div className="h-20 bg-element-base" />
     </>
