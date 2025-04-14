9baaf1e2d20d2204e3cfefabf492a1bb67b5ce65
alexsserban <alex.serban.dev@gmail.com>
2025-04-14 18:39:34 +0300

    feat: finish how-it-works section

--- apps/landing/src/app/home/_components/how-it-works/index.tsx
+++ apps/landing/src/app/home/_components/how-it-works/index.tsx
@@ -9,31 +9,29 @@ import { Step1Icon, Step2Icon, Step3Icon } from './assets';
 
 export default function HowItWorks() {
   return (
-    <Container className="w-fit justify-between gap-10 py-10 lg:w-auto lg:items-center lg:gap-20 lg:px-[6rem] lg:py-[10rem] xl:px-[8.75rem]">
-      <div className="flex flex-col gap-10 lg:items-center lg:gap-20">
-        <div className="flex flex-col gap-8 lg:items-center">
-          <p className="text-small text-primary lg:text-leading">How It Works</p>
+    <Container className="w-fit justify-between gap-20 py-10 sm:px-10 sm:py-20 md:w-[35rem] md:px-0 xl:w-auto xl:flex-row xl:items-start xl:px-[6.5rem] xl:py-[10rem] 2xl:px-[12rem]">
+      <div className="flex w-full flex-col gap-10 sm:items-center sm:gap-16 xl:gap-20">
+        <div className="flex flex-col gap-8 sm:items-center">
+          <p className="!font-heading text-small text-primary sm:text-leading">How It Works</p>
 
-          <div className="flex flex-col gap-4 lg:items-center">
-            <p className="max-w-[18.5rem] text-h6 text-primary lg:max-w-none lg:text-h2">
-              Earning Has Never Been Simpler
-            </p>
+          <div className="flex flex-col gap-4 sm:items-center">
+            <p className="text-h6 text-primary sm:text-center sm:text-h4 xl:text-h2">Earning Has Never Been Simpler</p>
 
-            <p className="max-w-96 text-leading text-secondary lg:max-w-[40.625rem] lg:text-center lg:text-smallSubheading">
+            <p className="max-w-[40rem] text-leading text-secondary sm:text-center sm:text-smallSubheading">
               Just deposit USDC and watch the value grow.” OR “Start earning steady, risk-adjusted returns today — just
               deposit USDC.
             </p>
           </div>
         </div>
 
-        <div className="grid gap-6 lg:grid-cols-3">
+        <div className="grid w-full gap-6 xl:grid-cols-3">
           <Card
             label="Step 1"
             title="Deposit"
             description="Deposit your stablecoins into Zivoe and receive czUSD."
             image={<Step1Icon />}
             className="items-end"
-            imageClassName="pt-4 items-end   lg:max-w-[70%]"
+            imageClassName="pt-4 items-end   sm:max-w-[70%]"
           />
 
           <Card
@@ -41,8 +39,8 @@ export default function HowItWorks() {
             title="Earn"
             description="czUSD automatically grows in value as interest is earned."
             image={<Step2Icon />}
-            className="items-end justify-start"
-            imageClassName="items-end pt-3 max-w-[20rem] lg:max-w-fit lg:[&>svg]:w-full"
+            className="w-full min-w-full flex-1 items-end justify-start"
+            imageClassName="items-end pt-3 max-w-[20rem] sm:w-full sm:max-w-max w-full flex-1 sm:[&>svg]:w-full"
           />
 
           <Card
@@ -51,7 +49,7 @@ export default function HowItWorks() {
             description="Swap your czUSD back for stablecoins at any time."
             image={<Step3Icon />}
             className="items-center"
-            imageClassName="max-w-[11rem] items-center lg:max-w-[70%]"
+            imageClassName="max-w-[11rem] items-center sm:max-w-[70%]"
           />
         </div>
       </div>
@@ -75,23 +73,23 @@ function Card({
   imageClassName?: string;
 }) {
   return (
-    <div className="shadow-[0px_1px_6px_-2px_rgba(18,19,26,0.08)] lg:max-w-full">
+    <div className="shadow-[0px_1px_6px_-2px_rgba(18,19,26,0.08)] sm:max-w-full">
       <div
         className={cn(
-          'flex h-[12.5rem] items-center justify-center bg-element-neutral-subtle lg:h-[18.75rem]',
+          'flex h-[12.5rem] items-center justify-center bg-element-neutral-subtle sm:h-[18.75rem]',
           className
         )}
       >
-        <div className={cn('flex h-full w-fit justify-center [&>svg]:h-full lg:[&>svg]:h-min', imageClassName)}>
+        <div className={cn('flex h-full w-fit justify-center [&>svg]:h-full sm:[&>svg]:h-min', imageClassName)}>
           {image}
         </div>
       </div>
-      <div className="flex flex-col gap-6 bg-element-neutral-light px-8 py-10 lg:gap-8">
-        <p className="!font-heading text-small text-primary lg:text-regular">{label}</p>
+      <div className="flex flex-col gap-6 bg-element-neutral-light px-8 py-10 sm:gap-8">
+        <p className="!font-heading text-small text-primary sm:text-regular">{label}</p>
 
         <div className="flex flex-col gap-3">
           <p className="!font-heading text-subheading text-primary">{title}</p>
-          <p className="max-w-[17.5rem] text-regular text-secondary lg:text-leading">{description}</p>
+          <p className="max-w-[17.5rem] text-regular text-secondary sm:text-leading">{description}</p>
         </div>
       </div>
     </div>
