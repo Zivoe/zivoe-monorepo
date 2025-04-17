2726da170ad9b617e80df1ff85832e4e32f508cb
alexsserban <alex.serban.dev@gmail.com>
2025-04-17 19:13:18 +0300

    feat: add right-tower

---
+++ apps/landing/src/app/home/_components/experience/assets/tower-right-tablet.tsx
@@ -0,0 +1,154 @@
+import React from 'react';
+
+import { IconProps } from '@zivoe/ui/icons/types';
+
+export const TowerRightTabletIcon = React.forwardRef<SVGSVGElement, IconProps>(
+  ({ color = 'currentColor', ...props }, forwardedRef) => {
+    return (
+      <svg
+        width="768"
+        height="838"
+        viewBox="0 0 768 838"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+        ref={forwardedRef}
+      >
+        <g style={{ mixBlendMode: 'luminosity' }}>
+          <g style={{ mixBlendMode: 'luminosity' }}>
+            <path
+              d="M914.847 485.994L792.64 467.193L756.912 395.983L631.023 377.311L541.876 428.509L417.192 409.234L326.841 463.445V553.424L267.417 586.371V1575.93L303.161 1651.08L424.502 1670.08L459.571 1742.94L583.718 1763.56L674.899 1708.85L799.747 1727.93L890.928 1673.04V1584.14L948.689 1546.5V559.319L914.847 485.994Z"
+              fill="#F3F4F6"
+            />
+          </g>
+          <g style={{ mixBlendMode: 'luminosity' }}>
+            <path
+              d="M914.847 485.994L792.64 467.193L756.912 395.983L631.023 377.311L541.876 428.509L417.192 409.234L326.841 463.445V553.424L267.417 586.371V1383.21L303.161 1458.36L424.502 1477.36L459.571 1550.22L583.718 1570.84L674.899 1516.14L799.747 1535.22L890.928 1480.32V1391.42L948.689 1353.78V559.319L914.847 485.994Z"
+              fill="url(#paint0_radial_10613_2953)"
+            />
+          </g>
+          <g style={{ mixBlendMode: 'luminosity' }}>
+            <path
+              d="M914.847 485.994L792.64 467.193L756.912 395.983L631.023 377.311L541.876 428.509L417.192 409.234L326.841 463.445L361.376 534.317L267.417 586.371L303.161 659.866L424.501 678.804L459.571 751.047L583.718 767.881L674.899 718.082L799.747 737.019L890.928 683.012L857.962 610.067L948.689 559.319L914.847 485.994Z"
+              fill="#FFF0E6"
+            />
+            <path
+              d="M914.847 485.994L792.64 467.193L756.912 395.983L631.023 377.311L541.876 428.509L417.192 409.234L326.841 463.445L361.376 534.317L267.417 586.371L303.161 659.866L424.501 678.804L459.571 751.047L583.718 767.881L674.899 718.082L799.747 737.019L890.928 683.012L857.962 610.067L948.689 559.319L914.847 485.994Z"
+              fill="#FFF0E6"
+              fillOpacity="0.4"
+            />
+          </g>
+          <g style={{ mixBlendMode: 'luminosity', opacity: '0.8' }}>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M699.383 519.492L792.639 467.192L822.19 539.225L733.815 591.83L857.961 610.066L767.482 662.67L641.233 644.434L733.815 591.831V591.83L699.383 519.493L573.897 499.948L482.716 552.591L517.085 626.198L424.501 678.802L393.282 606.377L482.716 552.59L361.375 534.315L449.751 482.412L573.897 499.947L541.876 428.508L665.78 448.745L699.383 519.492ZM517.085 626.199L641.231 644.435L674.898 718.08L550.751 697.038L517.085 626.199Z"
+              fill="url(#paint1_linear_10613_2953)"
+            />
+          </g>
+          <g style={{ mixBlendMode: 'luminosity', opacity: '0.1' }}>
+            <path
+              d="M792.642 467.193L699.386 519.493L733.818 591.831L641.234 644.435L517.088 626.199L550.755 697.039L641.234 712.375V644.435L733.818 657.808V591.831L792.642 556.816V467.193Z"
+              fill="url(#paint2_linear_10613_2953)"
+            />
+            <path
+              d="M541.879 428.509L665.783 448.746V514.259L573.9 499.948L541.879 428.509Z"
+              fill="url(#paint3_linear_10613_2953)"
+            />
+            <path
+              d="M449.754 482.413L361.378 534.316L454.424 548.329L482.719 552.591L393.285 606.378L424.504 678.803L484.452 644.742L482.719 552.591L454.424 548.329L449.754 482.413Z"
+              fill="url(#paint4_linear_10613_2953)"
+            />
+            <path
+              d="M733.818 591.831L641.234 644.435M733.818 591.831L699.386 519.493L792.642 467.193V556.816L733.818 591.831ZM733.818 591.831V657.808L641.234 644.435M641.234 644.435L517.088 626.199L550.755 697.039L641.234 712.375V644.435ZM482.719 552.591L454.424 548.329M482.719 552.591L393.285 606.378L424.504 678.803L484.452 644.742L482.719 552.591ZM454.424 548.329L361.378 534.316L449.754 482.413L454.424 548.329ZM541.879 428.509L665.783 448.746V514.259L573.901 499.948L541.879 428.509Z"
+              stroke="#C35928"
+            />
+          </g>
+          <g style={{ mixBlendMode: 'luminosity' }}>
+            <path
+              d="M792.642 467.192L699.386 519.493L733.818 591.83L641.234 644.435L733.818 657.808V591.83L792.642 556.816V467.192Z"
+              fill="#C35928"
+            />
+            <path
+              d="M449.754 482.413L361.378 534.316L454.424 548.329L482.719 552.59L393.285 606.378L424.504 678.803L484.452 644.741L482.719 552.59L454.424 548.329L449.754 482.413Z"
+              fill="#C35928"
+            />
+          </g>
+          <g style={{ mixBlendMode: 'luminosity' }} filter="url(#filter0_f_10613_2953)">
+            <ellipse cx="613.66" cy="570.441" rx="289.666" ry="169.773" fill="#E4F2FF" />
+          </g>
+        </g>
+        <defs>
+          <filter
+            id="filter0_f_10613_2953"
+            x="-76.0059"
+            y="0.667969"
+            width="1379.33"
+            height="1139.55"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="200" result="effect1_foregroundBlur_10613_2953" />
+          </filter>
+          <radialGradient
+            id="paint0_radial_10613_2953"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(608.053 579.932) rotate(90) scale(476.893 1035.96)"
+          >
+            <stop stopColor="#F9B480" />
+            <stop offset="1" stopColor="#E1C6B4" stopOpacity="0" />
+          </radialGradient>
+          <linearGradient
+            id="paint1_linear_10613_2953"
+            x1="609.668"
+            y1="445.215"
+            x2="609.668"
+            y2="718.08"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.0936512" stopColor="#E68F5B" />
+            <stop offset="1" stopColor="#CB916C" />
+          </linearGradient>
+          <linearGradient
+            id="paint2_linear_10613_2953"
+            x1="577.01"
+            y1="712.375"
+            x2="577.01"
+            y2="428.509"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#C35928" />
+            <stop offset="1" stopColor="#C35928" />
+          </linearGradient>
+          <linearGradient
+            id="paint3_linear_10613_2953"
+            x1="577.01"
+            y1="712.375"
+            x2="577.01"
+            y2="428.509"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#C35928" />
+            <stop offset="1" stopColor="#C35928" />
+          </linearGradient>
+          <linearGradient
+            id="paint4_linear_10613_2953"
+            x1="577.01"
+            y1="712.375"
+            x2="577.01"
+            y2="428.509"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#C35928" />
+            <stop offset="1" stopColor="#C35928" />
+          </linearGradient>
+        </defs>
+      </svg>
+    );
+  }
+);
