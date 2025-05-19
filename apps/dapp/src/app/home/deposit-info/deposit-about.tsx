6db7869e123e4a18a9c88d4a07b0600e1b543bcb
alexsserban <alex.serban.dev@gmail.com>
2025-05-19 18:32:07 +0300

    feat: add deposit about section

---
+++ apps/dapp/src/app/home/deposit-info/deposit-about.tsx
@@ -0,0 +1,14 @@
+import { ChartIcon, InfoIcon } from '@zivoe/ui/icons';
+
+import { DepositInfoSection } from './common';
+
+export default function DepositAbout() {
+  return (
+    <DepositInfoSection title="About" icon={<InfoIcon />}>
+      <p className="text-leading text-primary">
+        The Zivoe Credit Fund (zVLT) offers qualified purchasers exposure to a diversified portfolio of nonprime
+        consumer loans with 24/7 liquidity via secondary markets.
+      </p>
+    </DepositInfoSection>
+  );
+}
