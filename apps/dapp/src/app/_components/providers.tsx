7165378a07a42f4a2e6df97b44e54dd86d6e919c
alexsserban <alex.serban.dev@gmail.com>
2025-05-20 16:37:16 +0300

    feat: add wagmi

--- apps/dapp/src/app/_components/providers.tsx
+++ apps/dapp/src/app/_components/providers.tsx
@@ -6,9 +6,23 @@ import { useRouter } from 'next/navigation';
 
 import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
 import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
+import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-aria-components';
+import { sepolia } from 'viem/chains';
+import { WagmiProvider, createConfig, http, useAccount } from 'wagmi';
 
 import { env } from '@/env';
+
+const config = createConfig({
+  chains: [sepolia],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [sepolia.id]: http()
+  }
+});
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: ReactNode }) {
   const router = useRouter();
@@ -24,7 +38,11 @@ export default function Providers({ children }: { children: ReactNode }) {
           appName: 'Zivoe'
         }}
       >
-        {children}
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
+          </QueryClientProvider>
+        </WagmiProvider>
       </DynamicContextProvider>
     </RouterProvider>
   );
