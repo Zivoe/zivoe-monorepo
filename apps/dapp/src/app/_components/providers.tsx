6ab1d71f1da02b2978ab7b5eff994264ad96e493
alexsserban <alex.serban.dev@gmail.com>
2025-05-20 16:35:57 +0300

    feat: add connect wallet via dyamic

--- apps/dapp/src/app/_components/providers.tsx
+++ apps/dapp/src/app/_components/providers.tsx
@@ -4,10 +4,28 @@ import { ReactNode } from 'react';
 
 import { useRouter } from 'next/navigation';
 
+import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
 import { RouterProvider } from 'react-aria-components';
+
+import { env } from '@/env';
 
 export default function Providers({ children }: { children: ReactNode }) {
   const router = useRouter();
 
-  return <RouterProvider navigate={router.push}>{children}</RouterProvider>;
+  return (
+    <RouterProvider navigate={router.push}>
+      <DynamicContextProvider
+        settings={{
+          environmentId: env.NEXT_PUBLIC_DYNAMIC_ENV_ID,
+          walletConnectors: [EthereumWalletConnectors],
+          initialAuthenticationMode: 'connect-only',
+          networkValidationMode: 'always',
+          appName: 'Zivoe'
+        }}
+      >
+        {children}
+      </DynamicContextProvider>
+    </RouterProvider>
+  );
 }
