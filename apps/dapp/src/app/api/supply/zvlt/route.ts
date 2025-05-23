0056e65eb5c4f2c1cea37b8e7efe8861f0d8b34a
alexsserban <alex.serban.dev@gmail.com>
2025-05-23 16:03:25 +0300

    feat: add rate limiter to coingecko endpoint

--- apps/dapp/src/app/api/supply/zvlt/route.ts
+++ apps/dapp/src/app/api/supply/zvlt/route.ts
@@ -1,15 +1,44 @@
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+import { ipAddress } from '@vercel/functions';
 import { formatUnits } from 'viem';
 
 import { getContracts } from '@zivoe/contracts';
 import { zivoeVaultAbi } from '@zivoe/contracts/abis';
 
+import { redis } from '@/server/clients/redis';
 import { getWeb3Client } from '@/server/clients/web3';
 
 import { env } from '@/env';
 
-const handler = async () => {
-  const network = env.NEXT_PUBLIC_NETWORK;
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.fixedWindow(25, '30 m')
+});
 
+export const GET = async (req: Request) => {
+  const headers = new Headers();
+
+  // Verify CoinGecko headers
+  const requestedWith = req.headers.get('X-Requested-With');
+  const userAgent = req.headers.get('User-Agent');
+
+  if (requestedWith !== 'com.coingecko' || userAgent !== 'CoinGecko +https://coingecko.com/')
+    return Response.json({ message: 'Unauthorized request' }, { status: 403 });
+
+  // Rate limit based on IP address
+  const ip = ipAddress(req) ?? '127.0.0.1';
+  const identifier = `cg:${ip}`;
+
+  const rateLimitResult = await ratelimit.limit(identifier);
+  headers.set('X-RateLimit-Limit', rateLimitResult.limit.toString());
+  headers.set('X-RateLimit-Remaining', rateLimitResult.remaining.toString());
+
+  if (!rateLimitResult.success)
+    return Response.json({ message: 'The request has been rate limited.' }, { headers, status: 429 });
+
+  // Get zVLT supply
+  const network = env.NEXT_PUBLIC_NETWORK;
   const client = getWeb3Client(network);
   const contracts = getContracts(network);
 
@@ -20,8 +49,5 @@ const handler = async () => {
   });
 
   const result = formatUnits(supply, 18);
-
   return Response.json({ result });
 };
-
-export const GET = handler;
