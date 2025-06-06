52b58cbe976d2099c765c7aac69da53f6fbc7864
alexsserban <alex.serban.dev@gmail.com>
2025-06-06 17:38:38 +0300

    fix: improve dynamic io logic

--- apps/dapp/src/app/home/index.tsx
+++ apps/dapp/src/app/home/index.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import Hero from '@/components/hero';
 import Page from '@/components/page';
 
@@ -10,7 +12,10 @@ export default function Home() {
       <Hero title="zVLT" description="Gain exposure to non-prime consumer credit" />
 
       <Page className="flex gap-10 lg:flex-row">
-        <DepositInfo />
+        <Suspense>
+          <DepositInfo />
+        </Suspense>
+
         <Deposit />
       </Page>
     </>
