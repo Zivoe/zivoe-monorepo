5f2e5c70a254805d89d40a46198e6d3736cc0f31
alexsserban <alex.serban.dev@gmail.com>
2025-07-18 01:44:43 +0300

    TEMP

--- apps/dapp/src/app/home/deposit/index.tsx
+++ apps/dapp/src/app/home/deposit/index.tsx
@@ -7,18 +7,23 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@zivoe/ui/core
 
 import { depositDialogAtom } from '@/lib/store';
 
+import { useAccount } from '@/hooks/useAccount';
+
 import ConnectedAccount from '@/components/connected-account';
 
 import DepositBox from './deposit-box';
 
 export default function Deposit({ apy }: { apy: number }) {
   const [isDepositDialogOpen, setIsDepositDialogOpen] = useAtom(depositDialogAtom);
+  const { isPending, address } = useAccount();
 
   return (
     <>
       <DepositBox apy={apy} className="hidden lg:block lg:min-w-[30rem] xl:min-w-[39.375rem]" />
 
       <div className="fixed bottom-0 left-0 w-full border border-t border-default bg-surface-base p-4 lg:hidden">
+        {isPending ? 'isPending' : 'isNotPending'}
+        {address ? 'isConnected' : 'isNotConnected'}
         <ConnectedAccount>
           <Button fullWidth onPress={() => setIsDepositDialogOpen(true)}>
             Deposit
